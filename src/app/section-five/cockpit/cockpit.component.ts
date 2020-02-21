e8fbc799085520f7f5a615df5bbd51bd34b89847
Samoila Gabor <samoila.gabor@iquestgroup.com>
2020-02-21 12:19:52 +0200

    Use local reference for server content input field, fetched through VieuChild decorator

--- src/app/section-five/cockpit/cockpit.component.ts
+++ src/app/section-five/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,7 +9,7 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<any>();
 
-  newServerContent = 'some content';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
 
   constructor() { }
@@ -20,14 +20,14 @@ export class CockpitComponent implements OnInit {
   onAddServer(serverNameInput) {
     this.serverCreated.emit({
       serverName: serverNameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
   onAddServerBlueprint(serverNameInput) {
     this.blueprintCreated.emit({
       serverName: serverNameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
